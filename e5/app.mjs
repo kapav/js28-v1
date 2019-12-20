38e48dd5bc4ff95dfa4b570878aa87333f0c73ac
Кочкин Александр <Alexander.Kochkin@artezio.com>
2019-12-20 16:47:58 +0400

    Added mongoose package.

--- e5/app.mjs
+++ e5/app.mjs
@@ -1,9 +1,11 @@
 import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
 import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+// Импортировать модуль mongoose
+import mongoose from 'mongoose';
 
 import indexRouter from './routes/index.mjs';
 import usersRouter from './routes/users.mjs';
@@ -11,11 +13,21 @@ import usersRouter from './routes/users.mjs';
 const __dirname = path.dirname(new URL(import.meta.url).pathname).slice(3);
 const app = express();
 
+// Задание подключения к базе данных по умолчанию
+const mongoDB = 'mongodb://localhost:27017/my_database';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+// Предоставление mongoos'у возможности использовать глобальную библиотеку промисов
+mongoose.Promise = global.Promise;
+// Получение подключения по умолчанию
+const db = mongoose.connection;
+// Привязывание подключения к событию ошибки, чтобы получать сообщения об ошибках подключения
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +39,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, 'Введённая страница не найдена или не существует'));
 });
 
 // error handler
--- e5/routes/users.mjs
+++ e5/routes/users.mjs
@@ -7,4 +7,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* Маршрут для дополнительного сообщения */
+router.get('/cool', function(req, res, next) {
+  res.render('index', { title: 'Вид дополнительного сообщения' })
+})
+
 export default router;
