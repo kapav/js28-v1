6c21ee44b0d93e340bc5d65c2427dd9d966132a3
Кочкин Александр <Alexander.Kochkin@artezio.com>
2020-02-26 16:54:08 +0400

    Added the bookinstance and genre models, added elements into the database using an external script.

--- e5/models/genre.mjs
+++ e5/models/genre.mjs
@@ -1,3 +1,16 @@
-export default genre = function() {
-  console.log('Модель жанра')
-}
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const GenreSchema = new Schema({
+  name: {type: String, required: true, minlength: 3, maxlength: 100}
+})
+
+// Виртуальное свойство - url жанра
+GenreSchema
+  .virtual('url')
+  .get(function() {
+    return '/catalog/genre/' + this._id
+  })
+
+export default mongoose.model('Genre', GenreSchema)
