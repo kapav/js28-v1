bc93a28a68af4d00d9086770c5fb390e6e68d92f
Кочкин Александр <Alexander.Kochkin@artezio.com>
2020-03-17 10:10:12 +0400

    Updated the 'bookinstanceUpdateGet' method, redesigned the 'bookinstanceUpdatePost' method into a method array, updated the 'bookinstanceForm' view, added the 'Update' control to the 'bookinstanceDetail' view. Example 6-6.

--- e6/controllers/bookinstanceController.mjs
+++ e6/controllers/bookinstanceController.mjs
@@ -1,3 +1,4 @@
+import async from 'async'
 import validator from 'express-validator'
 
 import bookinstance from '../models/bookinstance.mjs'
@@ -83,7 +84,7 @@ export const bookinstanceCreatePost = [
             return
         }
         else {
-            // Данные из формы верны.
+            // Данные из формы верны. Сохранить экземпляр книги.
             currentBookinstance.save(function(err) {
                 if (err) { return next(err) }
                 // Экземпляр книги сохранён - перенаправить на страницу с информацией о нём.
@@ -117,11 +118,82 @@ export function bookinstanceDeletePost(req, res, next) {
 };
 
 // Показать форму обновления экземпляра книги по запросу GET.
-export function bookinstanceUpdateGet(req, res) {
-    res.send('Не реализовано: Обновление экземпляра книги по запросу GET');
+export function bookinstanceUpdateGet(req, res, next) {
+    // Запросить экземпляр книги и список книг для размещения в форме.
+    async.parallel({
+        bookinstance: function(callback) {
+            bookinstance.findById(req.params.id).populate('book').exec(callback)
+        },
+        books: function(callback) {
+            book.find(callback)
+        }
+    }, function(err, results) {
+        if (err) { return next(err) }
+        if (results.bookinstance === null) { // Результаты отсутствуют.
+            const err = new Error('Экземпляр книги не найден')
+            err.status = 404
+            return next(err)
+        }
+        // Успешное завершение, поэтому нужно отрисовать
+        res.render('bookinstanceForm', {
+            title: 'Обновить экземпляр книги',
+            bookList: results.books,
+            bookinstance: results.bookinstance
+        })
+    })
 };
 
 // Обновить экземпляр книги по запросу POST.
-export function bookinstanceUpdatePost(req, res) {
-    res.send('Не реализовано: Обновление экземпляра книги по запросу POST');
-};
+export const bookinstanceUpdatePost = [
+    // Проверить контролы.
+    validator.body('book', 'Книга должна быть указана.').trim().isLength({ min: 1 }),
+    validator.body('imprint', 'Выходные данные должны быть заполнены.').trim().isLength({ min: 1 }),
+    validator.body('dueBack', 'Неправильная дата.').optional({ checkFalsy: true }).isISO8601(),
+
+    // Очистить контролы.
+    validator.body('book').escape(),
+    validator.body('imprint').escape(),
+    validator.body('status').trim().escape(),
+    validator.body('dueBack').toDate(),
+
+    // Выполнить запрос после проверки и очистки.
+    (req, res, next) => {
+        // Извлечь ошибки проверки из запроса.
+        const errors = validator.validationResult(req)
+
+        // Добавить объект экземпляра книги со старым идентификатором, с заэкранированными данными, у которых также отсечены начальные и хвостовые пробелы.
+        const currentBookinstance = new bookinstance(
+            {
+                book: req.body.book,
+                imprint: req.body.imprint,
+                status: req.body.status,
+                dueBack: req.body.dueBack,
+                _id: req.params.id // Существующий идентификатор предотвращает создание нового.
+            })
+
+        if (!errors.isEmpty()) {
+            // Ошибки существуют. Отрисовать форму повторно с очищенными значениями и сообщениями об ошибке.
+            book.find({}, 'title')
+                .exec(function(err, books) {
+                    if (err) { return next(err) }
+                    // Успешное завершение, поэтому нужно отрисовать
+                    res.render('bookinstanceForm', {
+                        title: 'Обновить экземпляр книги',
+                        bookList: books,
+                        selectedBook: currentBookinstance.book._id,
+                        errors: errors.array(),
+                        bookinstance: currentBookinstance
+                    })
+                })
+            return
+        }
+        else {
+            // Данные из формы верны. Обновить экземпляр книги.
+            bookinstance.findByIdAndUpdate(req.param.id, currentBookinstance, {}, function(err, bookinstance) {
+                if (err) { return next(err) }
+                // Экземпляр книги обновлён обновлён - перенаправить на страницу со сведениями о нём.
+                res.redirect(bookinstance.url)
+            })
+        }
+    }
+];
